1120aa7a91d04b91aab6c427eaa6f1667d0f8817
Anthony Alperov <talperov10@gmail.com>
2025-10-27 18:01:38 -0700

    Add files via upload

---
+++ Alperov_PA7/Alperov_PA7/Data.cpp
@@ -0,0 +1,124 @@
+#include "Data.h"
+
+Data::Data()
+{
+	Number = 0;
+	ID = 0;
+	Name = "";
+	Email = "";
+	Units = "";
+	Program = "";
+	Level = "";
+	Absences_Num = 0;
+	Absences_Date;
+}
+
+Data::Data(int number, int id, const string& name, const string& email, const string& units, const string& program, const string& level)
+{
+	this->Number = number;
+	this->ID = id;
+	this->Name = name;
+	this->Email = email;
+	this->Units = units;
+	this->Program = program;
+	this->Level = level;
+	this->Absences_Num = 0;
+	Absences_Date;
+}
+
+Data::~Data()
+{
+	cout << "Destructor Activated" << endl; // Remember to delete this
+}
+
+int Data::getNumber() const
+{
+	return this->Number;
+}
+
+int Data::getID() const
+{
+	return this->ID;
+}
+
+string Data::getName() const
+{
+	return this->Name;
+}
+
+string Data::getEmail() const
+{
+	return this->Email;
+}
+
+string Data::getUnits() const
+{
+	return this->Units;
+}
+
+string Data::getLevel() const
+{
+	return this->Level;
+}
+
+int Data::getAbsences_Num() const
+{
+	return this->Absences_Num;
+}
+
+stack<string> Data::getAbsencesDate() const
+{
+	return this->Absences_Date;
+}
+
+
+void Data::setNumber(int number)
+{
+	this->Number = number;
+}
+
+void Data::setID(int id)
+{
+	this->ID = id;
+}
+
+void Data::setName(const string& name)
+{
+	this->Name = name;
+}
+
+void Data::setEmail(const string& email)
+{
+	this->Email = email;
+}
+
+void Data::setUnits(const string& units)
+{
+	this->Units = units;
+}
+
+void Data::setLevel(const string& level)
+{
+	this->Level = level;
+}
+
+void Data::setAbsences_Num(int num)
+{
+	this->Absences_Num = num;
+}
+
+void Data::pushAbsences_Date(const string& date)
+{
+	this->Absences_Date.push(date);
+	this->Absences_Num++;
+}
+
+string Data::peekAbsences_Date() const
+{
+	if (!this->Absences_Date.empty())
+	{
+		return this->Absences_Date.top();
+	}
+	return "No absences";
+}
+
---
+++ Alperov_PA7/Alperov_PA7/main.cpp
@@ -0,0 +1,24 @@
+/*
+Name: Anthony Alperov
+Date: 10/27/25
+Assingment: This is task PA7, goal of this assingment is to Attendace Tracker
+with FOUR major requirments.
+Requirment One: The application must import records pertaining to
+each student registered for the course from a course list.
+Requirment Two: The application must allow the user to mark each
+student in the course as present or absent on any given day.
+Requirment Three:The application must generate reports based on
+criteria.
+Requirment Four:The application must support a user interface to the
+attendance tracker.
+*/
+
+int main(void)
+{
+
+
+
+
+
+	return 0;
+}
